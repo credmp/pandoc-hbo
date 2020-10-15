4b5d7eab88fb29ad41a35704e2e02f1cf9f2352c
Arjen Wiersma <arjen@wiersma.org>
2020-10-15 15:33:58 +0200

    Een research rapport erbij

--- build-examples.sh
+++ build-examples.sh
@@ -16,3 +16,21 @@ pandoc examples/org-ref.org \
     --filter=pandoc-citeproc   \
     --biblio=meta/my-biblio.bib \
     --csl=meta/apa.csl
+
+echo "Building professional report"
+
+docker run -v $(pwd):/doc/ -t -i --rm credmp/docker-pandoc \
+pandoc examples/researchreport/rapport.org \
+       -o "examples/researchreport/document.pdf" \
+    -H meta/options.sty \
+    -N \
+    -B examples/researchreport/management.tex \
+    --metadata-file=examples/researchreport/preamble.yaml \
+    --from org \
+    --template "eisvogel.tex" \
+    --toc -V toc-own-page=true \
+    --filter=pandoc-latex-environment \
+    --filter=pandoc-crossref   \
+    --filter=pandoc-citeproc   \
+    --biblio=meta/my-biblio.bib \
+    --csl=meta/apa.csl
